310f67f36ce37a9035481c6e83462ed7fdf75324
Moises Sotelo <judz3699@outlook.com>
2023-11-26 22:54:34 -0600

    Fixed data processing and added training notebooks

--- Scripts Preprocessing/.ipynb_checkpoints/process_data-checkpoint.py
+++ Scripts Preprocessing/.ipynb_checkpoints/process_data-checkpoint.py
@@ -1,21 +1,21 @@
 # Import libraries
-import numpy as np
 from Bio import SeqIO
 from Bio.Seq import MutableSeq, Seq
 import pandas as pd
 import subprocess
 
-def process_vcf(data_path: str, chromosome: str, res_path: str):
+def generate_bed(data_path: str, chromosome: str, res_path: str):
     '''
     Function to process the data from the VCF files from Ensembl Variation.
-    Limited to single alternate alleles and SNPs.
+    Limited to single alternate alleles and SNPs. Check the `data_processing.ipynb` file.
 
-    Variables
-    ----------------------------------------------------------------------
-    data_path: path where the tsv files extracted by bcftools
-    from the original vcf files are stored
+    Variables:
+    ----------
+    data_path: path where the tsv files extracted by bcftools from the original vcf files are stored
 
-    res_path: 
+    res_path: path where the produced files will be saved
+
+    chromosome: number of chromosome to process
     '''
     
     data_path = data_path
@@ -31,6 +31,33 @@ def process_vcf(data_path: str, chromosome: str, res_path: str):
     chr_n_snps['end'] = chr_n_snps['pos'].astype(int) + 63
     chr_n_snps['bed'] = chr_n_snps['chr'].astype(str) + ':' + chr_n_snps['start'].astype(str) + '-' + chr_n_snps['end'].astype(str)
     chr_n_snps['bed'].to_csv('{}/bed_chr{}'.format(res_path, chromosome), sep='\t', index=False, header=False)
+    return chr_n_snps
 
+
+generate
     # This code has to be run directly in the terminal after adding the samtools path to the PATH variable
-    exit_code = subprocess.Popen("samtools faidx /mnt/sda1/Databases/Reference Genome/GRCh38p14/Ensembl/Homo_sapiens_GRCh38_dna_primary_assembly.fa -r {}/bed_chr{} -o {}/seq_vcf_chr{}".format(res_folder, res_folder), shell=True, stdout=subprocess.PIPE).stdout.read()
+    exit_code = subprocess.Popen("samtools faidx /mnt/sda1/Databases/Reference Genome/GRCh38p14/Ensembl/Homo_sapiens_GRCh38_dna_primary_assembly.fa -r {}/bed_chr{} -o {}/seq_vcf_chr{}".format(res_path, chromosome, res_path, chromosome), 
+                                 shell=True, stdout=subprocess.PIPE).stdout.read()
+    if str(exit_code,'utf-8')!='':
+        print(str(exit_code,'utf-8'))
+        exit(1)
+    
+    records = list(SeqIO.parse(res_path+'seq_vcf_chr{}'.format(chromosome), "fasta"))
+    ref_seqs = [sequence[1].seq for sequence in enumerate(records)]
+
+    alt_alleles = list(chr_n_snps['alt'])
+    alt_seqs = []
+    i = 0
+    while i < len(ref_seqs):
+        mutable = MutableSeq(ref_seqs[i])
+        mutable[64] = alt_alleles[i]
+        alt_seqs.append(str(mutable))
+        i+=1
+    
+    ref_seqs = list(map(str, ref_seqs)) # Turn Seq objects into normal Python Strings
+
+    chr_n_df = pd.DataFrame(data = chr_n_snps, copy=True)
+    chr_n_df['ref_seq'] = ref_seqs
+    chr_n_df['alt_seq'] = alt_seqs
+
+    return chr_n_df
--- Scripts Preprocessing/process_data.py
+++ Scripts Preprocessing/process_data.py
@@ -4,57 +4,82 @@ from Bio.Seq import MutableSeq, Seq
 import pandas as pd
 import subprocess
 
-def generate_sequences(data_path: str, chromosome: str, res_path: str):
+def generate_bed(chr_n_snps: pd.DataFrame, chromosome: str=None, res_path: str = None, gen_bed:bool = False):
     '''
     Function to process the data from the VCF files from Ensembl Variation.
-    Limited to single alternate alleles and SNPs. Check the `data_processing.ipynb` file.
+    Limited to single alternate alleles and SNPs. Check the `data_processing.ipynb` file for additional reference
 
     Variables:
     ----------
-    data_path: path where the tsv files extracted by bcftools from the original vcf files are stored
+    chr_n_snps: dataframe containing the variant registers (only SNVs)
 
-    res_path: path where the produced files will be saved
+    res_path: path where the produced files will be saved (required if gen_bed = True)
 
-    chromosome: number of chromosome to process
+    chromosome: number of chromosome to process (required if gen_bed = True)
+
+    gen_bed: A boolean value to indicate if the `bed` column will be exported into a csv file
     '''
     
-    data_path = data_path
-    res_path = res_path
-    chromosome = chromosome
-    chr_n_data = pd.read_csv(data_path+'chr{}_data.tsv'.format(chromosome), sep = '\t')
-    chr_n_data.rename(columns={'#[1]CHROM':'chr', '[2]POS':'pos', '[3]REF':'ref', '[4]ALT':'alt', '[5]TSA':'tsa', '[6]ID':'id'}, inplace=True)
-    
-    # Filter the variants that are snps and that have only one alternate allele
-    chr_n_snps = chr_n_data.loc[(chr_n_data['tsa']=='SNV') & (chr_n_data['alt'].str.len() == 1)]
-
-    chr_n_snps['start'] = chr_n_snps['pos'].astype(int) - 64
-    chr_n_snps['end'] = chr_n_snps['pos'].astype(int) + 63
+    # Create the list of alternative alleles
+    chr_n_snps["alt_list"] = chr_n_snps["alt"].str.split(pat=",")
+    chr_n_snps['start'] = chr_n_snps['pos'].astype(int) - 63
+    chr_n_snps['end'] = chr_n_snps['pos'].astype(int) + 64
     chr_n_snps['bed'] = chr_n_snps['chr'].astype(str) + ':' + chr_n_snps['start'].astype(str) + '-' + chr_n_snps['end'].astype(str)
-    chr_n_snps['bed'].to_csv('{}/bed_chr{}'.format(res_path, chromosome), sep='\t', index=False, header=False)
+    if gen_bed == True: chr_n_snps['bed'].to_csv('{}/bed_chr{}'.format(res_path, chromosome), sep='\t', index=False, header=False)
+    return chr_n_snps
 
-    # This code has to be run directly in the terminal after adding the samtools path to the PATH variable
-    exit_code = subprocess.Popen("samtools faidx /mnt/sda1/Databases/Reference Genome/GRCh38p14/Ensembl/Homo_sapiens_GRCh38_dna_primary_assembly.fa -r {}/bed_chr{} -o {}/seq_vcf_chr{}".format(res_path, chromosome, res_path, chromosome), 
-                                 shell=True, stdout=subprocess.PIPE).stdout.read()
-    if str(exit_code,'utf-8')!='':
-        print(str(exit_code,'utf-8'))
-        exit(1)
+
+
+
+
+def generate_sequences(variant_df: pd.DataFrame, chromosome: str, seq_path: str, bed_path: str=None, ref_genome_path:str=None,
+                       generate_fasta: bool = False):
+    '''
+    This function generates the reference and alternative sequences based on the information in variant_df and the reference genome. Calls samtools to do this
+    later task, and then saves the generated sequences in `gwas_associated_seq_path`.
+
+    Parameters:
+    ----------------
+    variant_df: a DataFrame containing the genomic variants information. These are the result of extracting the information from the original VCF files with bcftools.
+
+    ref_genome_path: ()
+
+    seq_path
+
+    bed_path
+
+    chromosome
+
+    generate_fasta
     
-    records = list(SeqIO.parse(res_path+'seq_vcf_chr{}'.format(chromosome), "fasta"))
+    '''
+    
+    if generate_fasta == True:
+        # This code has to be run directly in the terminal after adding the samtools path to the PATH variable
+        # These lines were extracted from the DeepPerVar paper
+    #-----------------------------------------------------------------------------------------------------#
+        exit_code = subprocess.Popen("samtools faidx {} -r {}/bed_chr{} -o {}/ref_seq_chr{}".format(ref_genome_path, bed_path,
+                                                                                                    chromosome, seq_path, chromosome),
+                                    shell=True, stdout=subprocess.PIPE).stdout.read()
+        if str(exit_code,'utf-8')!='':
+            print(str(exit_code,'utf-8'))
+            exit(1)
+
+    records = list(SeqIO.parse(seq_path+'/ref_seq_chr{}'.format(chromosome), "fasta"))
     ref_seqs = [sequence[1].seq for sequence in enumerate(records)]
+    ref_seqs = list(map(str, ref_seqs)) # Turn Seq objects into normal Python Strings and save them in a list
+    variant_df['ref_seq'] = ref_seqs
+    #-----------------------------------------------------------------------------------------------------#
+    # Generate the subsequences for variants, including the multiallelic ones
+    alt_seq = []
+    for idx, variant in enumerate(variant_df['alt_list']):
+        tmp_alt_seq = [] # Clear the contents of this list each time the for loop goes to a new register
+        for allele in variant:
+            tmp_seq = MutableSeq(variant_df['ref_seq'].iloc[idx])
+            tmp_seq[63] = allele
+            tmp_seq = str(tmp_seq)
+            tmp_alt_seq.append(tmp_seq)
+        alt_seq.append(tmp_alt_seq)
 
-    alt_alleles = list(chr_n_snps['alt'])
-    alt_seqs = []
-    i = 0
-    while i < len(ref_seqs):
-        mutable = MutableSeq(ref_seqs[i])
-        mutable[64] = alt_alleles[i]
-        alt_seqs.append(str(mutable))
-        i+=1
-    
-    ref_seqs = list(map(str, ref_seqs)) # Turn Seq objects into normal Python Strings
-
-    chr_n_df = pd.DataFrame(data = chr_n_snps, copy=True)
-    chr_n_df['ref_seq'] = ref_seqs
-    chr_n_df['alt_seq'] = alt_seqs
-
-    return chr_n_df
+    variant_df['alt_seq'] = alt_seq
+    return variant_df
